ebd06ec3205fce520638b10b9906b6595bc7ba3d
shouldsee <shouldsee@github.com>
2020-04-02 18:07:35 +0800

    make it possible to install from github tarball

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
 from setuptools.config import read_configuration
 from setuptools import setup
 config = read_configuration("setup.cfg")
-setup(use_scm_version=False, **config)
+setup(
+install_requires =
+	[
+    'lxml',
+    'pytz',
+    'requests',
+    ],
+
+setup_requires='''
+pytest-runner
+setuptools_scm
+wheel
+'''.strip().splitlines(),
+
+tests_require ='''
+mock
+pytest
+pytest-cov
+pyyaml
+tox
+vcrpy
+'''.strip().splitlines(),
+packages = ['src'],
+python_requires = ">= 3.6",
+zip_safe = True,
+package_dir = {'':'src'})
+
+	# **dict(config))
+
+
